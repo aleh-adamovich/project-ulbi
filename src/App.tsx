2574c56a15e554ae38eaab7abc43bacfb0faea7d
Aleh Adamovich <aleh.adamovich.v@gmail.com>
2023-10-17 23:26:13 +0300

    configure base routing + lazy loading for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import {lazy, Suspense, useEffect} from "react";
 import './index.scss';
-import {Counter} from "./components/Counter";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+
+const MainPage = lazy(() => import('./pages/MainPage/MainPage'));
+const AboutPage = lazy(() => new Promise((resolve) => {
+    // @ts-ignore
+    setTimeout(() => resolve(import('./pages/AboutPage/AboutPage')), 1000);
+}));
 
 export const App = () => {
     return (
         <div className="app">
-            <Counter/>
+            <BrowserRouter>
+                <Link to='/'>Main Page</Link>
+                <Link to='/about'>About Page</Link>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Routes>
+                        <Route path='/' element={<MainPage/>}/>
+                        <Route path='/about' element={<AboutPage/>}/>
+                    </Routes>
+                </Suspense>
+            </BrowserRouter>
         </div>
     )
 }
